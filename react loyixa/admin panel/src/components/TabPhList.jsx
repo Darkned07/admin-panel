f3c8ec51227c9dc05a9730b841e483b3280a30a7
Darkned07 <madaminovmuhammadyusuf806@gmail.com>
2024-11-19 21:36:34 +0500

    up

--- react loyixa/admin panel/src/components/TabPhList.jsx
+++ react loyixa/admin panel/src/components/TabPhList.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { Link, useNavigate } from "react-router-dom";
+import { FiEdit3 } from "react-icons/fi";
 
 function TabPhList({ lists, setDel }) {
   const navigate = useNavigate();
@@ -22,7 +23,7 @@ function TabPhList({ lists, setDel }) {
   };
   console.log(lists);
   return (
-    <div className="w-full lg:max-w-[600px] overflow-scroll h-[600px]">
+    <div className=" razmer overflow-scroll ">
       <table className="min-w-full divide-y divide-gray-200 ">
         <thead>
           <tr>
@@ -45,21 +46,18 @@ function TabPhList({ lists, setDel }) {
             lists.map((list) => {
               return (
                 <tr key={list.id}>
-                  <td className="px-6 py-4 whitespace-nowrap">{list.id}</td>
-                  <td className="px-6 py-4 whitespace-nowrap">{list.name}</td>
-                  <td className="px-6 py-4 whitespace-nowrap">{list.phone}</td>
-                  <td className="px-6 py-4 whitespace-nowrap">
-                    <Link
-                      to={`/edit/${list.id}`}
-                      className="px-2 py-1 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out"
-                    >
-                      s
+                  <td className="px-6 py-4 border-[1px] whitespace-nowrap">{list.id}</td>
+                  <td className="px-6 py-4 border-[1px] whitespace-nowrap">{list.name}</td>
+                  <td className="px-6 py-4 border-[1px] whitespace-nowrap">{list.phone}</td>
+                  <td className="px-6 py-4 border-[1px] whitespace-nowrap flex flex-row items-center gap-1">
+                    <Link to={`/edit/${list.id}`} className="btn btn-sm btn-outline">
+                      <FiEdit3 />
                     </Link>
                     <button
                       onClick={(e) => {
                         deleteUser(list.id);
                       }}
-                      className="ml-2 px-2 py-1 font-medium text-white bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out"
+                      className="btn btn-sm btn-warning btn-outline"
                     >
                       Delete
                     </button>
