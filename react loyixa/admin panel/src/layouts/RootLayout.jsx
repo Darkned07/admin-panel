7d6e6f05ac00fd519bcd6b8d37000739cf45f915
Darkned07 <madaminovmuhammadyusuf806@gmail.com>
2024-11-21 12:12:06 +0500

    update

--- react loyixa/admin panel/src/layouts/RootLayout.jsx
+++ react loyixa/admin panel/src/layouts/RootLayout.jsx
@@ -4,7 +4,7 @@ import Header from "../components/Header";
 function RootLayout() {
   return (
     <div className="lg:flex lg:flex-row  lg:items-start lg:justify-between ">
-      <header className="bg-[#288994] lg:bg-white">
+      <header className="">
         <Header />
       </header>
       <main>
